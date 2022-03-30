eef8d3d8b9004e9248a660a38c155a0296001d30
Tim Kuijpers <33220871+TJMKuijpers@users.noreply.github.com>
2022-03-30 19:23:29 +0200

    Add files via upload
    Updated css and index.html

--- server2.js
+++ server2.js
@@ -7,6 +7,7 @@ const url = 'mongodb://127.0.0.1:27017';
 const app = express();
 const router = express.Router();
 const bodyParser = require('body-parser');
+const { response } = require('express');
 // Configure Routing and port
 const port = 8080;// Listen to port
 app.listen(port);
@@ -21,27 +22,36 @@ MongoClient.connect(url, {
     if (err) {
         return console.log(err);
     }
-
+	const db = client.db('HeartValveDb');
+	
 	// Add routing to the server
 	app.get('/', (request,response)=>{
 		response.sendFile(path.join(__dirname, '/index.html'));
 	});
-
-
-	app.get('/', (request,response) =>{
-    // Specify database you want to access
-    const db = client.db('HeartValveDb');
-	const heartvalves = db.collection('heartvalves')
+	app.get('/start',(request,reponse)=>{
+		// Find all the documents in the database
+		var collections_in_db=db.getCollectionNames();
+		var count_db=[];
+		collections_in_db.forEach(function(collection_name){
+			count=db[collection_name].countDocuments();
+			count_db.push(count);
+		});
+		db_information=count_db;
+		// create an object to store collection name and count of document
+		return response.send(db_information);
+		console.log(db_information)
+		})
+	// get the overview of all documents when loading the page
 	
-	// Find one document in the heartvalve collection
-	heartvalves.find().toArray((err, results) => {
-		if (err){
-			return console.log(err);
-		}
-		else{
-			return console.log(results)
-		}
+	// Add a get request when data is searched
+	app.get('/search', (request,response)=>{
+		const collection_to_search =  db.collection('heartvalves');
+		collection_to_search.find.toArray((error,results)=>{
+			if (error){return console.log(error)}
+			else{ response.send(results)}
+		})
+
 	})
 });	
-}
-)
+
+
