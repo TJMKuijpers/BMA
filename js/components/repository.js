c0f56d6f597f010d2d153a486174be89a53dcb39
Tim Kuijpers <33220871+TJMKuijpers@users.noreply.github.com>
2022-04-08 17:08:10 +0200

    Add files via upload
    updated controller

--- js/components/repository.js
+++ js/components/repository.js
@@ -1,6 +1,19 @@
-myApp.component('repository',{
-    template:  '<h3>{{$ctrl.greeting}} Solar System!</h3>',
-    controller: function() {
-        this.greeting = 'Highlight the';
-    }
-})
+myApp.component('dataRepository',{
+    templateUrl:  '/js/directives/dataRepository.html',
+    controller: ['getData',function(getData) {
+        loadedData=getData.getjson('/data/BioMaterialAtlas.json');
+        loadedData.then(function(data){
+            console.log(data.data)
+            this.data=data.data;
+            console.log(this.data.ALP)
+        })
+        
+    }]
+}).factory('getData',function($http){
+    //return $resource('data/BioMaterialAtlas_StudyInformation.json')
+    return{
+        getjson : function(linktodata){
+           return $http.get(linktodata) // path of the json file
+        }}
+
+});
