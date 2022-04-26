d80492fb8fad5f6620495c6ac2b290b18e61df86
Tim Kuijpers <33220871+TJMKuijpers@users.noreply.github.com>
2022-04-26 16:49:39 +0200

    Add files via upload
    Further improved app.js

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
-const test = require('assert');
+var assert = require('assert');
 const bodyParser = require('body-parser');
 const {MongoClient}  = require("mongodb");
 const url = 'mongodb://localhost:27017/';
+const databaseName = 'testDB'
 // Server uses static files
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended:true}));
@@ -22,16 +23,25 @@ async function run() {
     // Connect the client to the server
     await client.connect();
     // Establish and verify connection
-    await client.db("testDB").command({ ping: 1 });
+    await client.db(databaseName).command({ ping: 1 });
     console.log("Connected successfully to server");
-    console.log(client.db("testDB").collection('customers').find({}));
+    client.db(databaseName).collection('topochipscreen').find({}).toArray(function(err,doc){
+      assert.equal(err,null);
+      return doc;
+    })
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    //await client.close();
   }
 }
+app.get('/atlas',(request,response,next)=>{
+    // get the data from the database
+    var data=run()
+    var data_json=data.toJSON();
+    console.log(data_json)
+    next();
+})
 
-run();
 // export the app so we can use it on our server
 module.exports = app;
 
