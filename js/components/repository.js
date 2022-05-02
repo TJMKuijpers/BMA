2af41499716a0157ab23bec7dbbd9f26c54eb101
Tim Kuijpers <33220871+TJMKuijpers@users.noreply.github.com>
2022-05-02 17:24:06 +0200

    Add files via upload
    added client side get request

--- js/components/repository.js
+++ js/components/repository.js
@@ -1,22 +1,14 @@
 myApp.component('dataRepository',{
     templateUrl:  '/js/directives/dataRepository.html',
-    controller: ['getData',function(getData,$scope) {
-        loadedData=getData.getjson('/data/BioMaterialAtlas.json');
-        loadedData.then(function(data){
-            var data_study=data.data;
-            $scope.data=data_study
-            console.log($scope.data)
-        })
+    controller: function($http,$scope) {
+        // get the data from the API /get-data
+        var dataSet = $http.get('/get-data').then(function(response){
+            var dataset=response.data;
+            console.log(dataset[0].Name)
+            return dataset;
+        });
         this.studycount=4;
-        this.celltypecount=10;
+        this.celltypecount=15;
         this.publications=4;
-
-        }]
-}).factory('getData',function($http){
-    //return $resource('data/BioMaterialAtlas_StudyInformation.json')
-    return{
-        getjson : function(linktodata){
-           return $http.get(linktodata) // path of the json file
-        }}
-
-});
+    }
+})
