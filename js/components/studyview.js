413846ba29ebd5444f042dfdfeb3a596af1e7588
Tim Kuijpers <33220871+TJMKuijpers@users.noreply.github.com>
2022-05-20 18:57:02 +0200

    Add files via upload
    Updated --> pass id to component

--- js/components/studyview.js
+++ js/components/studyview.js
@@ -1,15 +1,14 @@
 myApp.component('studyView',{
     templateUrl:  '/js/templates/topochip.html',
     controller: studyViewController,
-    bindings:{
-        /* pass the dataset from parent to this child element */
-      dataset:'<'
-    }
     
 });
 
-function studyViewController(){
-    this.testnaam='Tim',
+function studyViewController($scope,$rootScope,$state,$stateParams){
+    this.testnaam='Tim';
     this.testdata='08 Feb 1988'
-    console.log(this.dataset)
+    this.testid=$stateParams.ID;
+   
+    // Now we want to have the loaded data (MongoDB DB) in our scope
+
 }
