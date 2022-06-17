a100494ab867d7bee62f6f18eea4e7a763556fc1
Tim Kuijpers <33220871+TJMKuijpers@users.noreply.github.com>
2022-06-17 16:23:59 +0200

    Add files via upload
    Updated study selection

--- server2.js
+++ server2.js
@@ -13,7 +13,7 @@ const normalizePort = val => {
   return false;
 };
 // make sure a valid port is always provided (whether it is provided as a number or string)
-const port = normalizePort(process.env.PORT || '8080');
+const port = normalizePort(process.env.PORT || '8000');
 app.set('port', port);
 
 
@@ -51,58 +51,3 @@ server.on('listening', () => {
 server.listen(port);
 
 
-/*
-// Dependencies to launch the server
-var MongoClient = require('mongodb').MongoClient;
-var express = require('express');
-const path = require('path');
-const url = 'mongodb://127.0.0.1:27017';
-const app = express();
-const router = express.Router();
-const bodyParser = require('body-parser');
-const { response } = require('express');
-
-// Server uses static files
-app.use(express.static('public'));
-app.use(bodyParser.urlencoded({extended:true}));
-
-MongoClient.connect(url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}, (err, client) => {
-    if (err) {
-        return console.log(err);
-    }
-	const db = client.db('HeartValveDb');
-	
-	// Add routing to the server
-	app.get('/', (request,response)=>{
-		response.sendFile(path.join(__dirname, '/index.html'));
-	});
-	app.get('/start',(request,reponse)=>{
-		// Find all the documents in the database
-		var collections_in_db=db.getCollectionNames();
-		var count_db=[];
-		collections_in_db.forEach(function(collection_name){
-			count=db[collection_name].countDocuments();
-			count_db.push(count);
-		});
-		db_information=count_db;
-		// create an object to store collection name and count of document
-		return response.send(db_information);
-		console.log(db_information)
-		})
-	// get the overview of all documents when loading the page
-	
-	// Add a get request when data is searched
-	app.get('/search', (request,response)=>{
-		const collection_to_search =  db.collection('heartvalves');
-		collection_to_search.find.toArray((error,results)=>{
-			if (error){return console.log(error)}
-			else{ response.send(results)}
-		})
-
-	})
-});	
-
-*/
