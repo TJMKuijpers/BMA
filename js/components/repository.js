22f1494af698062aaed93df971cc3bcd793eb019
Tim Kuijpers <33220871+TJMKuijpers@users.noreply.github.com>
2022-08-23 15:25:28 +0200

    Add files via upload
    updated sidebar navigation

--- js/components/repository.js
+++ js/components/repository.js
@@ -51,6 +51,24 @@ myApp.component('dataRepository',{
         $scope.getTypeOf = function(item){
             return typeof item;
         }
+        $scope.updateCellTypes= function(){
+            console.log('checkbox is clicked')
+        }
+
+          var dropdown = document.getElementsByClassName("dropdown-btn");
+          var i;
+
+          for (i = 0; i < dropdown.length; i++) {
+            dropdown[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            var dropdownContent = this.nextElementSibling;
+            if (dropdownContent.style.display === "block") {
+                dropdownContent.style.display = "none";
+            } else {
+                dropdownContent.style.display = "block";
+            }
+            });
+            }
 
     }
 });
